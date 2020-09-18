d122903ecc043cbbc8a89d703678b80e267cbbcc
shreyash <Shreyash.pandey.katni@gmail.com>
2020-09-18 20:19:42 +0530

    Test Commit

---
+++ app/src/main/java/com/example/primecare/AuthenticationActivity.java
@@ -0,0 +1,54 @@
+package com.example.primecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.SignInUIOptions;
+import com.amazonaws.mobile.client.UserStateDetails;
+
+public class AuthenticationActivity extends AppCompatActivity {
+    private final String TAG = AuthenticationActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails result) {
+                Log.i(TAG, result.getUserState().toString());
+                switch (result.getUserState()){
+                    case SIGNED_IN:
+                        Intent i = new Intent(AuthenticationActivity.this, MainActivity.class);
+                        startActivity(i);
+                        break;
+                    case SIGNED_OUT:
+                        showSignIn();
+                        break;
+                    default:
+                        AWSMobileClient.getInstance().signOut();
+                        showSignIn();
+                        break;
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        });
+    }
+
+    private void showSignIn() {
+        try {
+            AWSMobileClient.getInstance().showSignIn(this, SignInUIOptions.builder().nextActivity(MainActivity.class).build());
+        }catch (Exception e){
+            Log.v(TAG, e.toString());
+        }
+    }
+}
