d76bf6ed345547812b97cca4f51757002bcdf465
shreyash <Shreyash.pandey.katni@gmail.com>
2020-09-22 13:42:22 +0530

    final commit
    Signed-off-by: shreyash <Shreyash.pandey.katni@gmail.com>

--- app/src/main/java/com/example/primecare/MainActivity.java
+++ app/src/main/java/com/example/primecare/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.primecare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mVideoCallBtn;
+    private Button mAiBtn;
+    private Button mMapBtn;
+    private Button mChatBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVideoCallBtn = findViewById(R.id.videoCall);
+        mMapBtn = findViewById(R.id.ambulanceBtn);
+        mChatBtn = findViewById(R.id.chatBtn);
+        Intent i = getIntent();
+        mVideoCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, videoCallActivity.class));
+            }
+        });
+        mChatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ChatActivity.class));
+            }
+        });
+        mMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=hospital");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//        mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
     }
 }
